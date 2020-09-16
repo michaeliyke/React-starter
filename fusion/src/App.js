f7db1d077348b7a5a79642856b2bf7ac693a8249
Michael C. Iyke <ymichaelc@gmail.com>
2020-09-16 10:39:38 +0100

    React Components part 2 - Lifting state up to common parent for shared access

--- fusion/src/App.js
+++ fusion/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from "reactstrap";
 import { Menu } from "./Components/MenuComponent";
 import './App.css';
+import { DISHES } from "./shared/dishes";
 
-function App() {
-  return (
+class App extends Component{
+
+  constructor(props) {
+    super(props);
+    /*
+    This is called state lifting. We lifted it from the child to the parent component
+    so that many children can have acces to it through the props attribute
+    */
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
     <div>
       <Navbar dark color="primary">
         <div className="container">
@@ -14,9 +28,10 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
   );
+  }
 }
 
 export default App;
