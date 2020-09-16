a5f9768b1e17ad8d43d500ed251abe8e921e633f
Michael C. Iyke <ymichaelc@gmail.com>
2020-09-16 11:28:56 +0100

    React Components part 2 - managing events

--- fusion/src/Components/MenuComponent.js
+++ fusion/src/Components/MenuComponent.js
@@ -5,14 +5,35 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "re
 export class Menu extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      selectedDish: null
+    };
+  }
+
+  onDishSelect(dish) {
+    this.setState({selectedDish: dish});
+  }
+
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+          <Card>
+            <CardImg width="100%" object src={dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle heading>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+              </Card>
+        );
+    } 
+      return (<div></div>);
   }
 
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card>
+          <Card onClick={() => this.onDishSelect(dish)}>
               <CardImg width="100%" object src={dish.image} alt={dish.name} />
             <CardImgOverlay>
       <CardTitle heading>{dish.name}</CardTitle>
@@ -25,6 +46,9 @@ export class Menu extends Component {
       <div className="container">
         <div className="row">
             {menu}
+        </div>
+        <div className="row">
+          {this.renderDish(this.state.selectedDish)}
         </div>
       </div>
     );
